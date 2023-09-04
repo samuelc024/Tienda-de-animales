1527a3a1cedb703ae547f00be1f0573bf13221b4
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-09-04 06:45:44 -0500

    Add files via upload

---
+++ tienda.py
@@ -0,0 +1,13 @@
+from animales import *
+from random import choice
+
+class Tienda:
+    def __init__(self):
+        self.animales=[]
+    
+    def agregar_animal(self, animal):
+        self.animales.append(animal)
+    
+    def entregar_animal(self):
+        return choice(self.animales)
+
